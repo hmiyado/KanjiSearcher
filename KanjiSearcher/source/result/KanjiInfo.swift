3049169cccc182d8e5c3c9c171d192449d90b738
miyado <hmiyado@gmail.com>
2020-07-26 17:57:35 +0900

    decode KanjiInfo

--- KanjiSearcher/source/result/KanjiInfo.swift
+++ KanjiSearcher/source/result/KanjiInfo.swift
@@ -2,29 +2,39 @@
 
 import Foundation
 
-struct KanjiInfo: Decodable {
+struct KanjiInfo: Equatable {
     /// MJ図形名
     var name: String
     /// 戸籍統一文字番号
     var number: Int
-    var type: KanjiType
-    var figure: KanjiFigure
+    //    var type: KanjiType
+    //    var figure: KanjiFigure
     /// 総画数
     var strokeCount: Int
-    var reading: KanjiReading
+    //    var reading: KanjiReading
+}
+
+extension KanjiInfo: Decodable {
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        name = try values.decode(String.self, forKey: .name)
+        number = Int(try values.decode(String.self, forKey: .number))!
+        strokeCount = try values.decode(Int.self, forKey: .strokeCount)
+    }
 
     enum CodingKeys: String, CodingKey {
-        case name = "MJ図形名"
+        case name = "MJ文字図形名"
         case number = "戸籍統一文字番号"
-        case type = "漢字施策"
-        case figure = "MJ文字図形"
+        //        case type = "漢字施策"
+        //        case figure = "MJ文字図形"
         case strokeCount = "総画数"
-        case reading = "読み"
+        //        case reading = "読み"
     }
+
 }
 
 /// 漢字施策
-struct KanjiType: Decodable {
+struct KanjiType: Decodable, Equatable {
     /// 人名用漢字
     var forPersonalName: Bool
     /// 常用漢字
@@ -37,14 +47,14 @@ struct KanjiType: Decodable {
 }
 
 /// MJ文字図形
-struct KanjiFigure: Decodable {
+struct KanjiFigure: Decodable, Equatable {
     var uri: URL
     /// MJ文字図形バージョン
     var version: String
 }
 
 /// 読み
-struct KanjiReading: Decodable {
+struct KanjiReading: Decodable, Equatable {
     /// 音読み
     var onyomi: [String]
     /// 訓読み
--- KanjiSearcher/source/result/KanjiResults.swift
+++ KanjiSearcher/source/result/KanjiResults.swift
@@ -2,11 +2,47 @@
 
 import Foundation
 
-struct KanjiResults: Decodable, Equatable {
+struct KanjiResults: Equatable {
     var status: KanjiResultStatus
     var find: Bool
     var count: Int
-    //    var results: [KanjiInfo]
+    var results: [KanjiInfo]
+}
+
+extension KanjiResults: Decodable {
+    init(from decorder: Decoder) throws {
+        let values = try decorder.container(keyedBy: CodingKeys.self)
+        status = try values.decode(KanjiResultStatus.self, forKey: .status)
+        find = try values.decode(Bool.self, forKey: .find)
+        count = try values.decode(Int.self, forKey: .count)
+
+        guard var resultsContainer = try? values.nestedUnkeyedContainer(forKey: .results) else {
+            results = []
+            return
+        }
+
+        var tempResults: [KanjiInfo] = []
+        while !resultsContainer.isAtEnd {
+            do {
+                let kanjiInfoDecoder = try resultsContainer.superDecoder()
+                let kanjiInfo = try KanjiInfo.init(from: kanjiInfoDecoder)
+                tempResults.append(kanjiInfo)
+            } catch {
+                // continue
+                print("Unexpected error: \(error).")
+            }
+        }
+
+        results = tempResults
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case status
+        case find
+        case count
+        case results
+    }
+
 }
 
 enum KanjiResultStatus: String, Codable {
