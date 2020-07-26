9c30df1ac5e20288226177e01cce0a17978d827f
miyado <hmiyado@gmail.com>
2020-07-26 20:45:23 +0900

    add implementation for Kanji search

--- KanjiSearcher/source/result/KanjiRepository.swift
+++ KanjiSearcher/source/result/KanjiRepository.swift
@@ -1,10 +1,15 @@
 //
 
 import Foundation
+import RxAlamofire
 import RxSwift
 
 class KanjiRepository {
-    func search(query: KanjiQuery) -> Observable<String> {
-        return .empty()
+    func search(query: KanjiQuery) -> Observable<KanjiResults> {
+        return RxAlamofire
+            .requestData(.get, query.asUrl()!)
+            .map { (resoibnse, data) in
+                KanjiResultConverter().convert(data)
+            }
     }
 }
