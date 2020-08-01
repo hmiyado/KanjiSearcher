76d8dbbfb680401eb3c0d1f0be98fe4264fefa68
miyado <hmiyado@gmail.com>
2020-08-01 13:26:53 +0900

    change Repository return type from Observable to Single

--- KanjiSearcher/source/result/KanjiRepository.swift
+++ KanjiSearcher/source/result/KanjiRepository.swift
@@ -5,11 +5,12 @@ import RxAlamofire
 import RxSwift
 
 class KanjiRepository {
-    func search(query: KanjiQuery) -> Observable<KanjiResults> {
+    func search(query: KanjiQuery) -> Single<KanjiResults> {
         return RxAlamofire
             .requestData(.get, query.asUrl()!)
             .map { (_, data) in
                 KanjiResultConverter().convert(data)
         }
+        .asSingle()
     }
 }
