01f00f99eab9d63edde15b4ff2ec92110083dd82
miyado <hmiyado@gmail.com>
2020-08-05 23:24:10 +0900

    show indicator while loading

--- KanjiSearcher/source/result/ResultViewController.swift
+++ KanjiSearcher/source/result/ResultViewController.swift
@@ -1,15 +1,32 @@
 //
 
 import UIKit
+import RxSwift
 
 class ResultViewController: UIViewController {
     private let viewModel: ResultViewModelType = ResultViewModel.init(kanjiRepository: KanjiRepository())
-    var query: KanjiQuery {
-        get { viewModel.input.onQuery.value }
-        set(value) { viewModel.input.onQuery.accept(value) }
-    }
-
+    var query: KanjiQuery = KanjiQuery.init()
+    private let disposableBag = DisposeBag()
+    @IBOutlet private weak var activityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
-        print(query ?? "no query")
+        viewModel
+            .output
+            .searchStatus
+            .drive(onNext: { [weak self] searchStatus in
+                switch searchStatus {
+                case .loading:
+                    self?.activityIndicator?.hidesWhenStopped = true
+                    self?.activityIndicator?.isHidden = false
+                    self?.activityIndicator?.startAnimating()
+                case .success(payload: _):
+                    self?.activityIndicator?.isHidden = true
+                case .error(error: _):
+                    self?.activityIndicator?.isHidden = true
+                }
+            })
+            .disposed(by: disposableBag)
+        
+        viewModel.input.onQuery.accept(query)
     }
 }
