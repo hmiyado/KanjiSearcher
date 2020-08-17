d82bab81383ae7f85c495cd2ec1eba05ea00fe2e
miyado <hmiyado@gmail.com>
2020-08-17 23:55:14 +0900

    show error view at center of screen

--- KanjiSearcher/source/result/ResultViewController.swift
+++ KanjiSearcher/source/result/ResultViewController.swift
@@ -9,6 +9,7 @@ class ResultViewController: UIViewController {
     private let disposableBag = DisposeBag()
     @IBOutlet private weak var activityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var errorView: UIStackView!
+    @IBOutlet weak var containerView: UIView!
     
     init?(coder: NSCoder, query: KanjiQuery) {
         self.query = query
@@ -24,18 +25,24 @@ class ResultViewController: UIViewController {
             .output
             .searchStatus
             .drive(onNext: { [weak self] searchStatus in
+                guard let self = self else {
+                    return
+                }
                 switch searchStatus {
                 case .loading:
-                    self?.activityIndicator?.hidesWhenStopped = true
-                    self?.activityIndicator?.isHidden = false
-//                    self?.errorView?.removeFromSuperview()
-                    self?.activityIndicator?.startAnimating()
+                    self.activityIndicator?.hidesWhenStopped = true
+                    self.activityIndicator?.isHidden = false
+                    self.errorView?.removeFromSuperview()
+                    self.activityIndicator?.startAnimating()
                 case .success(payload: _):
-                    self?.activityIndicator?.isHidden = true
+                    self.activityIndicator?.isHidden = true
                 case .error(error: _):
-//                    self?.activityIndicator?.isHidden = true
-                    if let errorView = self?.errorView {
-//                        self?.view?.addSubview(errorView)
+                    self.activityIndicator?.isHidden = true
+                    if let errorView = self.errorView, let containerView = self.containerView {
+                        containerView.addSubview(errorView)
+                        errorView.translatesAutoresizingMaskIntoConstraints = false
+                        errorView.centerXAnchor.constraint(equalToSystemSpacingAfter: containerView.centerXAnchor, multiplier: 1.0).isActive = true
+                        errorView.centerYAnchor.constraint(equalToSystemSpacingBelow: containerView.centerYAnchor, multiplier: 1.0).isActive = true
                     }
                 }
             })
